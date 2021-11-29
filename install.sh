4251b3aa39626c48fa883ebfd0884219bd9f8e3a
Dave Tapley <dave@tapley.com>
2021-11-28 18:09:05 -0700

    Use pwd
    Inspired by
    https://github.com/benmatselby/dotfiles/blob/main/install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,4 @@
 #!/usr/bin/env bash
 
-if [[ -z "$CODESPACES" ]]; then
-    cat "${HOME}/dotfiles/.bashrc" >>  "${HOME}/.bashrc"
-    cp "${HOME}/dotfiles/.gitconfig" "${HOME}"
-else
-    cat "/workspaces/.codespaces/.persistedshare/dotfiles/.bashrc" >>  "${HOME}/.bashrc"
-    cp "/workspaces/.codespaces/.persistedshare/dotfiles/.gitconfig" "${HOME}"
-fi
+cat "$(pwd)/.bashrc" >>  "${HOME}/.bashrc"
+cat "$(pwd)/.gitconfig" >> "${HOME}/.gitconfig"
